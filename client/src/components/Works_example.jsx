d93cecaa33d79f8cf53546f8952bee95aaabaf4a
grockwix <gprodsoftcomm@gmail.com>
2025-07-16 16:29:16 +0300

    gprod_1.0.1(change video)

--- client/src/components/Works_example.jsx
+++ client/src/components/Works_example.jsx
@@ -1,25 +1,35 @@
-import work_1 from '/assets/work_1.mp4'
-import work_2 from '/assets/work_2.mov'
-import work_3 from '/assets/work_3.mov'
+import work_1 from '../../assets/work_1.mp4'
+import work_2 from '../../assets/work_2.mp4'
+import work_3 from '../../assets/work_3.mp4'
 
-export function Works_example() {
+function Video({ href, src }) {
+  return (
+    <a
+      className='video_link'
+      href={href}
+      target='_blank'
+      rel='noopener noreferrer'
+    >
+      <video muted autoPlay loop src={src}></video>
+    </a>
+  )
+}
+
+function Works_example() {
+  const videos = [work_1, work_2, work_3]
+  const links = [
+    'https://vimeo.com/889608599',
+    'https://vimeo.com/890283588',
+    'https://vimeo.com/1009634929',
+  ]
+
   return (
     <nav className='works_example'>
-      <Video
-        className='first'
-        src={work_3}
-        href='https://vimeo.com/1009634929'
-      />
-      <Video src={work_1} href='https://vimeo.com/889608599' />
-      <Video src={work_2} href='https://vimeo.com/890283588' />
+      {videos.map((video, index) => (
+        <Video key={index} href={links[index]} src={video} />
+      ))}
     </nav>
   )
 }
 
-function Video({ className, src, href }) {
-  return (
-    <a className={className} href={href} target='_blank'>
-      <video className={className} src={src} autoPlay loop muted />
-    </a>
-  )
-}
+export { Works_example }
